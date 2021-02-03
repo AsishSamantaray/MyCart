3246dc1dcabea1c065130879f620904ca9fbc40b
Asish Samantaray <samantaray.asish04@gmail.com>
2021-02-03 16:57:42 +0530

    Login Activity UI designed.

--- app/src/main/java/com/asish/mycart/activities/LoginActivity.kt
+++ app/src/main/java/com/asish/mycart/activities/LoginActivity.kt
@@ -2,11 +2,14 @@ package com.asish.mycart.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.asish.mycart.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
