12ab42ecdd9d8bd46f7cabd07f350452d312e447
Asish Samantaray <samantaray.asish04@gmail.com>
2021-02-03 19:25:04 +0530

    UI Improved.

--- app/src/main/java/com/asish/mycart/activities/LoginActivity.kt
+++ app/src/main/java/com/asish/mycart/activities/LoginActivity.kt
@@ -1,8 +1,7 @@
 package com.asish.mycart.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.asish.mycart.R
 
 class LoginActivity : AppCompatActivity() {
